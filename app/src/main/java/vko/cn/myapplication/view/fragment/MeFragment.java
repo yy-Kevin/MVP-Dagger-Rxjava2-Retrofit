c809d7b2fce937991c8ee42bf796fda1694a2113
yuyao <380712098@qq.com>
2017-09-11 09:29:05 +0800

    add root

--- app/src/main/java/vko/cn/myapplication/view/fragment/MeFragment.java
+++ app/src/main/java/vko/cn/myapplication/view/fragment/MeFragment.java
@@ -1,5 +1,6 @@
 package vko.cn.myapplication.view.fragment;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -7,12 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import vko.cn.myapplication.R;
+import vko.cn.myapplication.databinding.FragmentMeBinding;
 
 /**
  * Created by A on 2017/9/8.
  */
 
 public class MeFragment extends BaseFragment {
+
+    private FragmentMeBinding binding;
 
     public static MeFragment getInstance() {
         MeFragment fragment = new MeFragment();
@@ -22,7 +26,8 @@ public class MeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.from(getActivity()).inflate(R.layout.fragment_me,null);
-        return view;
+//        View view = inflater.from(getActivity()).inflate(R.layout.fragment_me,null);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_me, container, false);
+        return binding.getRoot();
     }
 }
